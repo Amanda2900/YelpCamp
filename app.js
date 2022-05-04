7ef409900022882986357a3b9388b5dba60db6db
Amanda DiNoto <amanda.dinoto@gmail.com>
2022-05-04 14:29:56 -0400

    create campground model and configure database to connect to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
 const path = require('path');
+
+require('./config/database.js');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
