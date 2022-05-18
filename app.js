14df6dfb8c0a2ccc0f73839a72a95358a613eced
Amanda DiNoto <amanda.dinoto@gmail.com>
2022-05-18 12:56:32 -0400

    add catchAsync and ExpressError utility functions for basic error catching and added these onto routes in app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const catchAsync = require('./utilities/catchAsync');
 const methodOverride = require('method-override');
 
 require('./config/database.js');
@@ -31,33 +32,37 @@ app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new');
 })
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', { campground });
-});
+}));
 
-app.get('/campgrounds/:id/edit', async(req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async(req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/edit', { campground });
-});
+}));
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', catchAsync(async (req, res, next) => {
   const campground = new Campground(req.body);
   await campground.save();
   res.redirect(`campgrounds/${campground._id}`);
-});
+}));
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body }, { new: true });
   res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds');
-})
+}));
+
+app.use((err, req, res, next) => {
+  res.send('Oh no, something went wrong');
+});
 
 app.listen(3000, () => {
   console.log('Serving on port 3000');
