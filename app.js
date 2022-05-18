91f8046584aaadac4b899f9cb91894fdb9a48eb1
Amanda DiNoto <amanda.dinoto@gmail.com>
2022-05-18 14:15:16 -0400

    install joi for error handling and use joi schema to validate on routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate');
+const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utilities/catchAsync');
 const ExpressError = require('./utilities/ExpressError');
 const methodOverride = require('method-override');
@@ -18,6 +19,15 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if(error) {
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400);
+  }
+  next();
+};
 
 
 app.get('/', (req, res) => {
@@ -43,14 +53,13 @@ app.get('/campgrounds/:id/edit', catchAsync(async(req, res) => {
   res.render('campgrounds/edit', { campground });
 }));
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-  if(!req.body.campground) throw new ExpressError("Invalid campground data", 400);
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
   const campground = new Campground(req.body);
   await campground.save();
   res.redirect(`campgrounds/${campground._id}`);
 }));
 
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, { ...req.body }, { new: true });
   res.redirect(`/campgrounds/${campground._id}`);
