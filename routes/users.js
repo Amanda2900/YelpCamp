74c27643a6159dbe1fd59d24733d48b413010bc9
Amanda DiNoto <amanda.dinoto@gmail.com>
2022-05-24 17:36:48 -0400

    add login routes and form

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utilities/catchAsync');
 const User = require('../models/user');
 
@@ -15,10 +16,18 @@ router.post('/register', catchAsync(async (req, res) => {
     req.flash('success', 'Welcome to Yelp Camp!');
     res.redirect('/campgrounds');
   } catch(e) {
-    console.log(e)
     req.flash('error', e.message);
     res.redirect('register');
   }
 }));
+
+router.get('/login', (req, res) => {
+  res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login'}), (req, res) => {
+  req.flash('success', `Welcome back!`);
+  res.redirect('/campgrounds')
+});
 
 module.exports = router;
