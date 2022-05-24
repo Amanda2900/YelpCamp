69e6453cc08446ec806f5d5731ddb1bddc14ec34
Amanda DiNoto <amanda.dinoto@gmail.com>
2022-05-24 19:23:09 -0400

    add returnTo behavior

--- middleware.js
+++ middleware.js
@@ -3,6 +3,7 @@ const ExpressError = require('./utilities/ExpressError');
 
 module.exports.isLoggedIn = (req, res, next) => {
   if(!req.isAuthenticated()) {
+    req.session.returnTo = req.originalUrl;
     req.flash('error', 'You must be logged in.');
     return res.redirect('/login');
   }
