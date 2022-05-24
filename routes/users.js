3e27fe95fe3d20279734bcbbdb308d354a0703a4
Amanda DiNoto <amanda.dinoto@gmail.com>
2022-05-24 19:16:14 -0400

    add login to register route

--- routes/users.js
+++ routes/users.js
@@ -14,8 +14,11 @@ router.post('/register', catchAsync(async (req, res) => {
     const { email, username, password } = req.body;
     const user = new User({ email, username });
     const registeredUser = await User.register(user, password);
-    req.flash('success', 'Welcome to Yelp Camp!');
-    res.redirect('/campgrounds');
+    req.login(registeredUser, err => {
+      if(err) return next(err);
+      req.flash('success', 'Welcome to Yelp Camp!');
+      res.redirect('/campgrounds');
+    })
   } catch(e) {
     req.flash('error', e.message);
     res.redirect('register');
@@ -33,12 +36,10 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
 
 router.get('/logout', (req, res) => {
   req.logout(function(err) {
-    if(err) {
-      return next(err);
-    }
+    if(err) return next(err);
+    req.flash('success', 'Goodbye!');
+    res.redirect('/campgrounds');
   });
-  req.flash('success', 'Goodbye!');
-  res.redirect('/campgrounds');
 })
 
 module.exports = router;
