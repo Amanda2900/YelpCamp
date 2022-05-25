69b6af1356e4322d581d0af7506baeb5c99e8f54
Amanda DiNoto <amanda.dinoto@gmail.com>
2022-05-25 14:32:47 -0400

    add authorization for reviews and show author name on review

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const Campground = require('./models/campground.js');
+const Review = require('./models/review.js');
 const { campgroundSchema, reviewSchema } = require('./schemas');
 const ExpressError = require('./utilities/ExpressError');
 
@@ -34,6 +35,16 @@ module.exports.isAuthor = async (req, res, next) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   if (!campground.author.equals(req.user._id)) {
+    req.flash('error', "You don't have permission to do that.");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
     req.flash('error', "You don't have permission to do that.");
     return res.redirect(`/campgrounds/${id}`);
   }
