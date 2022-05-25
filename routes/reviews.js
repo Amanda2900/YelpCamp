1317a0de81fcd750b9dfd7e8ac1ad796ffd59c8c
Amanda DiNoto <amanda.dinoto@gmail.com>
2022-05-25 15:13:44 -0400

    clean up user route by adding a users controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-const reviews = require('../controllers/review');
+const reviews = require('../controllers/reviews');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
 
