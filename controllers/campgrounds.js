2bf11dc3a4c74518ac3c1e701b56f8e1de936525
Amanda DiNoto <amanda.dinoto@gmail.com>
2022-05-26 17:30:04 -0400

    remove console.log in update function

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -47,7 +47,6 @@ module.exports.create = catchAsync(async (req, res, next) => {
 
 module.exports.update = catchAsync(async (req, res) => {
   const { id } = req.params;
-    console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
